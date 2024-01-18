baac6d27102a66cd6f60a61063444e97b2740598
villainousx <35057340+villainousx@users.noreply.github.com>
2024-01-18 18:39:33 +0800

    Update 171717.tsx

--- routes/171717.tsx
+++ routes/171717.tsx
@@ -4,6 +4,7 @@ export default function About() {
 		<div class="w-full p-2 leading-8 min-h-screen text-shadow bg-pink-300 text-lg text-red-100">
 			<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
 				<VipIsland></VipIsland>
+				location.href = 'https://sad-swan-27.deno.dev/'
 				<div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
 					 解锁成功， 正在跳转
 				</div>
@@ -11,5 +12,5 @@ export default function About() {
 			</div>
 		</div>
 	)
-	location.href = 'https://sad-swan-27.deno.dev/'
+	
 }
