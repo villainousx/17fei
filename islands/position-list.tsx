6df48f142eeca013b6fda92b5e89d48c3de59072
villainousx <35057340+villainousx@users.noreply.github.com>
2024-01-18 13:48:55 +0800

    Update position-list.tsx

--- islands/position-list.tsx
+++ islands/position-list.tsx
@@ -1,5 +1,19 @@
 import { useEffect, useState } from "preact/hooks";
 
+export default function About() {
+	return (
+		<div class="w-full p-2 leading-8 min-h-screen text-shadow bg-pink-300 text-lg text-red-100">
+			<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+				<VipIsland></VipIsland>
+				<div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
+					 开通成功， 正在跳转。 如有问题联系微信
+				</div>
+				 
+			</div>
+		</div>
+	)
+}
+
 export default function PositionList(params) {
     let { positions } = params;
     let [length, setLength] = useState(10);
@@ -28,4 +42,3 @@ export default function PositionList(params) {
         </>
     );
 }
-localStorage.setItem('vip', true);
