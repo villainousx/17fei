cb13433fc16cf181122acb27288334898b8d633c
villainousx <35057340+villainousx@users.noreply.github.com>
2024-01-18 19:33:11 +0800

    Update 171717.tsx

--- routes/171717.tsx
+++ routes/171717.tsx
@@ -13,7 +13,16 @@ export default function About() {
 			</div>
 		</div>
 	)
-	useEffect(()=>{
-		 location.href = 'https://sad-swan-27.deno.dev/'
-	},[])	
+
 }
+ 
+const RedirectComponent = () => {
+  // 在组件加载时执行跳转操作
+  useEffect(() => {
+    window.location.href = '/'; // 设置新页面的 URL
+  }, []);
+  
+  return null; // 不需要显示任何内容，因此直接返回空元素
+};
+ 
+export default RedirectComponent;
