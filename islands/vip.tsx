224a8db1c9d6cb707d5d201ddd01815a780eaa57
villainousx <35057340+villainousx@users.noreply.github.com>
2024-01-18 18:07:08 +0800

    Update vip.tsx

--- islands/vip.tsx
+++ islands/vip.tsx
@@ -11,11 +11,11 @@ interface Props {
 export default function VipIsland({ children }: Props) {
    useEffect(()=>{
 
-    if(IS_BROWSER){
+
       localStorage.setItem('lockVip', JSON.stringify({"type":"boolean","data":true}));
       localStorage.setItem('vip', true);
 
-    }
+
       return ()=>{}
    },[])
 
