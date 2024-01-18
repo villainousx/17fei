7cce6ee9250368b4a7ceb1fa0a1cc7552d0dadaf
villainousx <35057340+villainousx@users.noreply.github.com>
2024-01-18 18:24:57 +0800

    Update 171717.tsx

--- routes/171717.tsx
+++ routes/171717.tsx
@@ -1,8 +1,14 @@
 import VipIsland from "../islands/vip.tsx";
 export default function About() {
 	return (
-
+		<div class="w-full p-2 leading-8 min-h-screen text-shadow bg-pink-300 text-lg text-red-100">
+			<div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
 				<VipIsland></VipIsland>
-		
+				<div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
+					 解锁成功， 正在跳转
+				</div>
+				 
+			</div>
+		</div>
 	)
 }
