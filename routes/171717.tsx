e96a3f8c20044bcf1afc55055c5d8032fe9abdc9
villainousx <35057340+villainousx@users.noreply.github.com>
2024-01-18 14:02:15 +0800

    Update 171717.tsx

--- routes/171717.tsx
+++ routes/171717.tsx
@@ -3,11 +3,6 @@ export default function About() {
 	return (
 
 				<VipIsland></VipIsland>
-				<div class="leading-8 text-gray-900 max-w-lg mt-8 max-auto">
-					 开通成功， 正在跳转。 
-				</div>
-				 
-			</div>
-		</div>
+		
 	)
 }
